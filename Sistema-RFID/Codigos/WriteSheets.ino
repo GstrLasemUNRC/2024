6686a913d77924228daddd6f71694624db57856e
luciantu-a <lucia.antuna15@gmail.com>
2024-04-30 12:44:43 -0300

    Create WriteSheets.ino

---
+++ Sistema-RFID/Codigos/WriteSheets.ino
@@ -0,0 +1,49 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include "HTTPSRedirect.h"
+
+#include "ConstantesRFID.h"
+#include "funcionesRFID.h"
+
+
+void setup() {
+
+  Serial.begin(9600);        
+  delay(10);
+  Serial.println('\n');
+  
+  // Conexion WiFi
+  config_wifi();
+  // Conexion con Google Sheets
+  config_google();
+
+
+}
+
+
+void loop() {
+
+  // SECCION :  ENVIAR DATOS A Google Sheets.
+  // se generan valores de prueba
+  value0 ++;
+  value1 = random(0,1000);
+  value2 = random(0,100000);
+
+  // Se construye una cadena JSON payload que contiene los valores
+  snprintf(payload, sizeof(payload), "%s\"%d\",\"%d\",\"%d\"}", payload_base, value0, value1, value2); //  "%s\"%d,%d,%d\"}"
+  
+  // Publicacion de datos en Google Sheets
+  Serial.println("Puublicando datos...");
+  Serial.println(payload);
+  if(client->POST(url, host, payload)){ 
+    // si la publicación fue exitosa ...
+    Serial.println("Publicación exitosa");
+  }
+  else{
+    Serial.println("Error al publicar datos");
+  }
+  // se requiere un retraso de varios segundos antes de volver a publicar   
+  delay(5000);
+
+  // FIN DE LA SECCION :  ENVIAR DATOS A Google Sheets.
+}
