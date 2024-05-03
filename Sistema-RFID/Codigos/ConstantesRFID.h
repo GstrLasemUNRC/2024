c037fd297b55f5572849259338985294e231d3f7
luciantu-a <lucia.antuna15@gmail.com>
2024-05-03 19:33:39 -0300

    Update ConstantesRFID.h

--- Sistema-RFID/Codigos/ConstantesRFID.h
+++ Sistema-RFID/Codigos/ConstantesRFID.h
@@ -12,16 +12,14 @@ const int ledPin = 13;
 #define PN532_RESET (3) 
 
 const int NUM_BYTES_TO_COPY = 4; // utilizado para imprimir y almacenar los primeros 4 bytes del UID
-uint8_t firstFourBytes[NUM_BYTES_TO_COPY];
+//uint8_t firstFourBytes[NUM_BYTES_TO_COPY];
 #define MAX_UID_LENGTH 4 // Suponiendo una longitud máxima de UID de 7 bytes
 uint8_t uid[MAX_UID_LENGTH]; // Declarar uid como un arreglo de uint8_t
 uint8_t uidLength; // Longitud del UID leído
 
 
-
-
 // Google Script Deployment ID:
-const char *GScriptId = "AKfycbzlKrX1HpVaIELflssnzJEMn0PbvIMzhorwMtnpKtn9bTy-zBl-2_3fMXCHOHWGQZxVDQ";
+const char *GScriptId = "AKfycbw1-kF0LZHfgsDMbRvofVdkw2akuNpwD3DwXVGRKpKFXBqnbYCkQFu48HjDpFIOplFCSg"; // v2: AKfycbyDBhZ5k8jpslE58SScIxL-gU20A18lDLkyJKnQT7MKRLfXSH40ranEPfTZXVGIeFbsvA"
 
 // Enter command (insert_row or append_row) and your Google Sheets sheet name (default is Sheet1):
 const char* payload_base = "{\"command\": \"insert_row\", \"datosPerros\": \"Hoja1\", \"UIDtag\", \"IDdog\", \"Nombre\": ";
@@ -34,3 +32,11 @@ const char* fingerprint = "";
 const String url = String("/macros/s/") + GScriptId + "/exec";
 HTTPSRedirect* client = nullptr;
 
+// Sheet y json
+String uidString = "";
+String response = "";
+int row = 0;
+int fila = 0;
+int Nombredog = 3;
+int Edaddog = 4;
+String valorLeido = "";
